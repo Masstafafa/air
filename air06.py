ebc9c35941d76d12fee09b8ab38b65facf3449f7
Pierre Favier <pierrefavier26@gmail.com>
2025-02-07 12:21:26 +0100

    en cours

---
+++ air06.py
@@ -0,0 +1,29 @@
+import sys
+
+# Fonctions utilisées:
+
+        
+# Gestion d'erreurs :
+
+def is_valid_length(arguments): #vérifie qu'il y a bien deux argumens au moins
+    if len(arguments) < 2:
+        print("Erreur : Merci d'indiquer au moins deux arguments entre guillemets")
+        return False
+    return True
+
+# Récupération de données :
+def get_arguments():
+    arguments = sys.argv[1:]
+    return arguments
+
+# Résolution :
+
+def get_result():
+
+
+
+# Affichage :
+def display():
+ 
+
+display()
