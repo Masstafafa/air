c0b002dba76d448ff92741688d8cc67f6033b190
Pierre Favier <pierrefavier26@gmail.com>
2025-02-18 12:10:46 +0100

    fonction variables et annotations

--- air05.py
+++ air05.py
@@ -1,11 +1,10 @@
 import sys
 
 # Fonctions utilisées:
-def make_addition_or_substraction(numbers, addorsub):
+def make_addition_or_substraction(numbers: list[str], addorsub: str) -> list[int]:
     operator = addorsub[0]
     operator_number = int(addorsub[1:]) 
     new_list_number = []
-
     for number in numbers:
         number = int(number)
         if operator == "-":
@@ -17,56 +16,61 @@ def make_addition_or_substraction(numbers, addorsub):
         
 # Gestion d'erreurs :
 
-def is_valid_length(arguments): #vérifie qu'il y a bien deux argumens au moins
+def is_valid_length(arguments: list[str]) -> bool: #vérifie qu'il y a bien deux argumens au moins
     if len(arguments) < 2:
         print("Erreur : Merci d'indiquer au moins deux nombres entiers")
         return False
     return True
 
-def is_valid_argument(arguments): #vérifie que les données rentrées par l'utilisateur sont bien des chiffres
-    for arg in arguments[:-1]:
-        if not arg.isdigit(): 
+def is_valid_argument(arguments: list[str]) -> bool: #vérifie que les données rentrées par l'utilisateur sont bien des chiffres
+    for argument in arguments[:-1]:
+        if not argument.isdigit(): 
             print("Erreur : Merci d'indiquer uniquement des nombres entiers")
             return False
     return True
 
-def is_valid_operator(last_argument): #vérifie que l'on fait bien une addition ou soustraction avec un chiffre
+def is_valid_operator(last_number: str) -> bool: #vérifie que l'on fait bien une addition ou soustraction
     list_operator = ["-", "+"]
-    if last_argument[0] not in list_operator:
+    if last_number[0] not in list_operator:
         print("Erreur : Merci d'indiquer - ou + devant le dernier argument")
         return False
+    return True
     
-    if not last_argument[1:].isdigit():
+def is_valid_number(last_number: str) -> bool: #verifie que le dernier element soit bien un chiffre
+    if not last_number[-1:].isdigit():
         print("Erreur : Le nombre après le signe doit être un entier")
         return False
     return True
 
 
 # Récupération de données :
-def get_arguments():
+def get_arguments() -> list[str]:
     arguments = sys.argv[1:]
     return arguments
 
 # Résolution :
 
-def get_result():
+def display_result() -> None:
     arguments = get_arguments()
     if not is_valid_length(arguments):
         return
     if not is_valid_argument(arguments):
         return
-    if not is_valid_operator(arguments[-1]):
+    
+    last_number = arguments[-1]
+    if not is_valid_operator(last_number):
         return
-    return make_addition_or_substraction(arguments[:-1], arguments[-1])
+    if not is_valid_number(last_number):
+        return
+    
+    numbers = arguments[:-1]
+    result = make_addition_or_substraction(numbers, last_number)
+    for number in result:
+        print(number, end=' ')
 
+        #ou print(" ".join(map(str, result)))
 
 # Affichage :
-def display():
-    result = get_result()
-    if result is not None:
-        for num in result:
-            print(num, end=' ')
 
-display()
-
+display_result()
     
