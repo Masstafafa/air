50921d34ffb5bebc1bd9af949dd9f2235942115e
Pierre Favier <pierrefavier26@gmail.com>
2025-02-18 10:42:04 +0100

    modif nom variable fonction annotation

--- air04.py
+++ air04.py
@@ -1,40 +1,42 @@
 import sys
 
 # Fonctions utilisées:
-def find_repetitive_character(arguments):
+
+def remove_consecutive_character(string: str) -> str:
     new_string = '' #Création d'une variable pour mettre la phrase
-    for i in range(len(arguments) -1): #longueur de la phrase
-        current_chr = arguments[i]
-        next_chr = arguments[i + 1]
+    for i in range(len(string) -1): #longueur de la phrase
+        current_chr = string[i]
+        next_chr = string[i + 1]
         if current_chr != next_chr:
             new_string += current_chr
-        
-    new_string += arguments[-1] #dernière lettre
+    new_string += string[-1] #dernière lettre
     return new_string
 
 # Gestion d'erreurs :
-def is_valid_length(arguments):
+
+def is_valid_length(arguments: list[str]) -> bool:
     if len(arguments) != 1:
         print("Erreur : Merci d'indiquer une seule phrase entre guillemets")
         return False
     return True
 
 # Récupération de données :
-def get_arguments():
+
+def get_arguments() -> list[str]:
     arguments = sys.argv[1:]
     return arguments
 
 # Résolution :
-def no_repetitive_character():
+
+def display_no_consecutive_character() -> None:
     arguments = get_arguments()
     if not is_valid_length(arguments):
         return
-    return find_repetitive_character(arguments[0])
+    
+    string = arguments[0]
+    final_string = remove_consecutive_character(string)
+    print(final_string)
 
 # Affichage :
-def display():
-    final_string = no_repetitive_character()
-    if final_string is not None:  
-        print(final_string)
 
-display()
+display_no_consecutive_character()
