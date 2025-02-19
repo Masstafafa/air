f938d42eab957ecdb1d58680cbd3ceaede5b90aa
Pierre Favier <pierrefavier26@gmail.com>
2025-02-19 09:47:20 +0100

    ok

--- air06.py
+++ air06.py
@@ -1,61 +1,53 @@
-'''Créez un programme qui supprime d’un tableau tous les éléments qui ne contiennent pas une autre chaîne de caractères.
-
-Utilisez une fonction de ce genre (selon votre langage) :
-ma_fonction(array_de_strings, string) {
-	# votre algorithme
-	return (nouvel_array_de_strings)
-}
-
-
-Exemples d’utilisation :
-$> python exo.py “Michel” “Albert” “Thérèse” “Benoit” “t”
-Michel
-
-$> python exo.py “Michel” “Albert” “Thérèse” “Benoit” “a”
-Michel, Thérèse, Benoit
-
-
-
-Afficher error et quitter le programme en cas de problèmes d’arguments.
-'''
-
 import sys
 
 # Fonctions utilisées:
-def remove_last_array(strings, remove_string):
+def remove_last_array(strings: list[str], remove_string: str) -> list[str]:
     new_string = [] #creation d'une variable pour la nouvelle phrase
-
-    for word in strings:  
-        if remove_string not in word:  
+    
+    for word in strings:
+        found = False    # Pour indiquer si on trouve remove_string dans le mot
+        
+        # On ne vérifie que jusqu'à la position qui permet de contenir remove_string
+        for i in range(len(word) - len(remove_string) + 1):
+            match = True 
+            
+            # On vérifie chaque caractère de remove_string
+            for j in range(len(remove_string)):
+                if word[i + j] != remove_string[j]:
+                    match = False
+                    break
+            if match:
+                found = True
+                break
+        if not found:
             new_string.append(word)
-    return new_string
+        return new_string
 
 # Gestion d'erreurs :
 
-def is_valid_length(arguments): #vérifie qu'il y a bien deux argumens au moins
+def is_valid_length(arguments: list[str]) -> bool: #vérifie qu'il y a bien deux argumens au moins
     if len(arguments) < 2:
         print("Erreur : Merci d'indiquer au moins deux arguments entre guillemets")
         return False
     return True
 
 # Récupération de données :
-def get_arguments():
+def get_arguments() -> list[str]:
     arguments = sys.argv[1:]
     return arguments
 
 # Résolution :
 
-def get_removed_array():
+def print_removed_array() -> None:
     arguments = get_arguments()
     if not is_valid_length(arguments):
         return
-    return remove_last_array(arguments[:-1], arguments[-1])
+    
+    strings = arguments[:-1]
+    remove_string = arguments[-1]
+    new_array = remove_last_array(strings, remove_string)
+    print(" ".join(map(str, new_array)))
 
 # Affichage :
-def display():
-    new_array = get_removed_array()
-    if new_array is not None:
-        for word in new_array:
-            print(word, end=' ')
- 
-display()
+
+print_removed_array()
