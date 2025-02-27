f8506054f36ae7a5ec89ca35ae8996efe0c89e14
Pierre Favier <pierrefavier26@gmail.com>
2025-02-27 12:07:12 +0100

    ok

--- air14.py
+++ air14.py
@@ -1,22 +1,24 @@
 import sys
+# Fonctions utilitaires :
 
-# Fonctions utilisées:
-
-
+def get_answer():
+    with open('aaa.txt', 'r') as file:
+        contenu = file.read()
+    return contenu
 
 # Gestion d'erreurs :
 
 def is_valid_length(arguments):
-    if len(arguments) != 1:
-        print("Erreur : Merci d'indiquer un seul argument")
+    if len(arguments) != 0:
+        print("Erreur : Merci de n'indiquer que le nom du programmme sans argument")
         return False
     return True
     
 # Récupération de données :
 
-def get_arguments():
+def get_arguments() -> list[str]:
     arguments = sys.argv[1:]
-    return arguments  
+    return arguments
 
 # Résolution :  
 
@@ -24,7 +26,8 @@ def display_answer():
     arguments = get_arguments()
     if not is_valid_length(arguments):
         return
-    answer = arguments[0]
+
+    answer = get_answer()
     print(f"J’ai terminé l’Épreuve de l’air et c’était {answer}")
 
 # Affichage :
